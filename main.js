1b9017fb3d7c833bed8b3f482e7da08f6d7c3fe4
FrancesMutahi <francesnjambimutahi@gmail.com>
2022-06-13 23:45:50 +0300

    getting started with js basics

---
+++ main.js
@@ -0,0 +1,27 @@
+// (var), let - you can reassign values, const = cannot reassign. Always use const unless you know you are going to reuse the values
+
+//let age = 30;
+//age = 31 //this is reassigning
+
+//console.log(age);
+
+
+//datatypes: strings, numbers, boolen, null, undefined, symbols
+
+//const name = 'Frances' //you can use single or double quotes. semi-colon is optional
+//const age = 24;
+//const rating = 4.5 //this datatype is just a number
+//const isCool = true
+//const x = null
+//const y = undefined //undefined can be explicitly defined
+//let z //undefined
+
+//console.log(typeof z)
+
+//Concantenation
+//console.log('my name is '+ name + ' and I am' +age)
+
+//Template string, use backticks(funny first key under escape)
+//const hello = `my name is ${name} and I am ${age}`
+
+//console.log(hello)
