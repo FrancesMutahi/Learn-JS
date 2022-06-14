f88f192f58a934c794a69d8dc8c430025580a8cb
FrancesMutahi <francesnjambimutahi@gmail.com>
2022-06-14 06:49:50 +0300

    arrays

--- main.js
+++ main.js
@@ -28,10 +28,37 @@
 
 //string properties and methods
 
-const s = 'Hello world!'
+//const s = 'Hello world!'
 
-console.log(s.length) //a property does not have parenthesis
-console.log(s.toUpperCase()) //a method has parenthesis.A method is a function associated with an object
-console.log(s.substring(0, 5).toLowerCase()) //takes two indexes, start and finish
-console.log(s.split(''))
+//console.log(s.length) //a property does not have parenthesis
+//console.log(s.toUpperCase()) //a method has parenthesis.A method is a function associated with an object
+//console.log(s.substring(0, 5).toLowerCase()) //takes two indexes, start and finish
+//console.log(s.split(''))
+/* multiline comments*/
+
+//Arrays variables that hold multiple values
+
+//const numbers = new Array(1, 2, 3, 4, 5) //using a constructor. The keyword for constructors is new
+//console.log(numbers)
+
+const fruits = ['apples', 'mangoes', 'bananas', 10, true] //commonly used method. Arrays can have different datatypes
+
+fruits[5] = 'oranges' //adding a value to the array
+
+fruits.push('passion') //adding a value to the tail
+
+fruits.unshift('strawberries')// adding a value to the start
+
+fruits.pop() //deleting the last value
+
+console.log(Array.isArray(fruits)) //check presence
+
+console.log(fruits.indexOf('oranges')) //check index in array
+
+console.log(fruits)
+
+console.log(fruits[1])
+
+
+
 
